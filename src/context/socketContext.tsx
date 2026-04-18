8ed6e5bde3a68d98901aee0d75d82a0bc14c8203
Ujjwal Singh <ujzzzwall@gmail.com>
2026-04-18 22:04:03 +0530

    setup basic frontend for webrtc project

---
+++ src/context/socketContext.tsx
@@ -0,0 +1,21 @@
+import SocketIoClient from 'socket.io-client'
+import React, {  createContext } from 'react'
+
+const WS_Server = "http://localhost:5500"
+
+const SocketContext = createContext<any | null>(null);
+
+const socket = SocketIoClient(WS_Server); // creating connection from react server to node server
+
+
+interface Props {
+    children: React.ReactNode
+}
+
+export const SocketProvider :React.FC<Props> = ({children})=>{
+    return (
+        <SocketContext.Provider value={{socket}}>
+        {children}
+        </SocketContext.Provider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SocketProvider } from './context/socketContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <SocketProvider>
     <App />
-  </StrictMode>,
+  </SocketProvider>
 )
